46a858beec9f6d4a936575e79b409fc69b0f3ed0
Hanno Schlichting <hanno@jarn.com>
2009-07-27 15:15:46 +0000

    Really disable all of linkintegrity by overriding the event handlers.

--- Products/Extropy/patches.py
+++ Products/Extropy/patches.py
@@ -5,3 +5,13 @@ from App import FactoryDispatcher
 
 FactoryDispatcher._product_packages = \
     forever.memoize(FactoryDispatcher._product_packages)
+
+
+from plone.app.linkintegrity import handlers
+
+def _null_handler(obj, event):
+    return
+
+handlers.modifiedArchetype = _null_handler
+handlers.referenceRemoved = _null_handler
+handlers.referencedObjectRemoved = _null_handler
