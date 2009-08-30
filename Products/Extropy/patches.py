227011fa1c70f1e3efbd08936578d3a8629f6786
Hanno Schlichting <hanno@jarn.com>
2009-08-30 18:19:24 +0000

    Welcome to the Jungle - add more monkeys.

--- Products/Extropy/patches.py
+++ Products/Extropy/patches.py
@@ -15,3 +15,53 @@ def _null_handler(obj, event):
 handlers.modifiedArchetype = _null_handler
 handlers.referenceRemoved = _null_handler
 handlers.referencedObjectRemoved = _null_handler
+
+
+def opaqueItems(self):
+    """ We don't use no freaking comments """
+    return ()
+
+from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
+
+# We don't have any opaque items in the site, ignore those
+CMFCatalogAware.originalOpaqueItems = CMFCatalogAware.opaqueItems
+CMFCatalogAware.opaqueItems = opaqueItems
+
+
+# The transform chain path doesn't really change, so it's likely to be
+# the same for the combination of source and target mime types
+from plone.memoize.forever import memoize
+from Products.PortalTransforms.TransformEngine import TransformTool
+TransformTool._findPath = memoize(TransformTool._findPath)
+
+
+from plone.memoize import volatile
+
+from Acquisition import ImplicitAcquisitionWrapper
+from Products.Archetypes import BaseObject
+from Products.Archetypes.interfaces import ISchema
+
+
+_SCHEMA_CACHE = {}
+
+def _schema_storage(fun, *args, **kwargs):
+    return _SCHEMA_CACHE
+
+
+def _schema_key(method, self, *args, **kwargs):
+    return (self.__class__, self.portal_type)
+
+
+@volatile.cache(_schema_key, _schema_storage)
+def _raw_schema(self):
+    return ISchema(self)
+
+
+def _Schema(self):
+    """Return a (wrapped) schema instance for this object instance.
+    """
+    schema = _raw_schema(self)
+    return ImplicitAcquisitionWrapper(schema, self)
+
+
+BaseObject.BaseObject.Schema = _Schema
