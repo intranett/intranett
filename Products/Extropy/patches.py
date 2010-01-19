e678fbde60e1db6b1a71ef71b36d570a3ef080cc
Hanno Schlichting <hanno@jarn.com>
2010-01-19 14:22:15 +0000

    Use the saver version of the schema cache, just for the request duration

--- Products/Extropy/patches.py
+++ Products/Extropy/patches.py
@@ -26,35 +26,3 @@ from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
 # We don't have any opaque items in the site, ignore those
 CMFCatalogAware.originalOpaqueItems = CMFCatalogAware.opaqueItems
 CMFCatalogAware.opaqueItems = opaqueItems
-
-
-from plone.memoize import volatile
-
-from Acquisition import ImplicitAcquisitionWrapper
-from Products.Archetypes import BaseObject
-from Products.Archetypes.interfaces import ISchema
-
-
-_SCHEMA_CACHE = {}
-
-def _schema_storage(fun, *args, **kwargs):
-    return _SCHEMA_CACHE
-
-
-def _schema_key(method, self, *args, **kwargs):
-    return (self.__class__, self.portal_type)
-
-
-@volatile.cache(_schema_key, _schema_storage)
-def _raw_schema(self):
-    return ISchema(self)
-
-
-def _Schema(self):
-    """Return a (wrapped) schema instance for this object instance.
-    """
-    schema = _raw_schema(self)
-    return ImplicitAcquisitionWrapper(schema, self)
-
-
-BaseObject.BaseObject.Schema = _Schema
