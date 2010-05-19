5d95988fd0b1bb8d7ee2b0ea2630a781d37f45ea
Hanno Schlichting <hanno@jarn.com>
2010-05-19 16:59:27 +0000

    Some patches we don't need anymore

--- Products/Extropy/patches.py
+++ Products/Extropy/patches.py
@@ -1,12 +1,3 @@
-from plone.memoize import forever
-
-# Remember the installed products and packages
-from App import FactoryDispatcher
-
-FactoryDispatcher._product_packages = \
-    forever.memoize(FactoryDispatcher._product_packages)
-
-
 from plone.app.linkintegrity import handlers
 
 def _null_handler(obj, event):
@@ -15,14 +6,3 @@ def _null_handler(obj, event):
 handlers.modifiedArchetype = _null_handler
 handlers.referenceRemoved = _null_handler
 handlers.referencedObjectRemoved = _null_handler
-
-
-def opaqueItems(self):
-    """ We don't use no freaking comments """
-    return ()
-
-from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
-
-# We don't have any opaque items in the site, ignore those
-CMFCatalogAware.originalOpaqueItems = CMFCatalogAware.opaqueItems
-CMFCatalogAware.opaqueItems = opaqueItems
