7a76f5cced9ed1565cf85d37e9ecd6a0ff63c300
Hanno Schlichting <hanno@jarn.com>
2010-08-06 09:37:44 +0000

    Clean up the test skeleton

--- jarn/extranet/tests.py
+++ jarn/extranet/tests.py
@@ -1,29 +1,31 @@
 import unittest
 
-from zope.testing import doctestunit
-from zope.component import testing
-from Testing import ZopeTestCase as ztc
-
 from Products.Five import zcml
 from Products.Five import fiveconfigure
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import PloneSite
+from Testing import ZopeTestCase as ztc
 ptc.setupPloneSite()
 
 import jarn.extranet
 
-class TestCase(ptc.PloneTestCase):
-    class layer(PloneSite):
-        @classmethod
-        def setUp(cls):
-            fiveconfigure.debug_mode = True
-            zcml.load_config('configure.zcml',
-                             jarn.extranet)
-            fiveconfigure.debug_mode = False
+class ExtranetLayer(PloneSite):
+    @classmethod
+    def setUp(cls):
+        fiveconfigure.debug_mode = True
+        zcml.load_config('configure.zcml',
+                         jarn.extranet)
+        fiveconfigure.debug_mode = False
+        ztc.installPackage('jarn.extranet')
 
-        @classmethod
-        def tearDown(cls):
-            pass
+    @classmethod
+    def tearDown(cls):
+        pass
+
+
+class TestCase(ptc.PloneTestCase):
+
+    layer = ExtranetLayer
 
 
 def test_suite():
