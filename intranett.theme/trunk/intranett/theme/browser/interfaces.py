0a966a43f0ec7136868b5ab5d00ea151b4a1d60e
Denys Mishunov <denys@jarn.com>
2010-08-20 14:11:22 +0000

    a lot of styles, re-factoring of the templates and initial implementation of jBreadCrumb

--- intranett.theme/trunk/intranett/theme/browser/interfaces.py
+++ intranett.theme/trunk/intranett/theme/browser/interfaces.py
@@ -1,6 +1,10 @@
 from plone.theme.interfaces import IDefaultPloneLayer
-
+from zope.viewlet.interfaces import IViewletManager
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope browser layer.
     """
+    
+class IAboveColumns(IViewletManager):
+    """A viewlet manager that sits above the columns
+    """    
