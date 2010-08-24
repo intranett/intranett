2c5e308235b9357670001b52abf8734f8cc2298b
Denys Mishunov <denys@jarn.com>
2010-08-24 14:59:13 +0000

    Just a lot of stuff - personal bar, breadcrumbs, forms etc

--- intranett.theme/trunk/intranett/theme/browser/interfaces.py
+++ intranett.theme/trunk/intranett/theme/browser/interfaces.py
@@ -8,3 +8,9 @@ class IThemeSpecific(IDefaultPloneLayer):
 class IAboveColumns(IViewletManager):
     """A viewlet manager that sits above the columns
     """    
+    
+class ITopBar(IViewletManager):
+    """A viewlet manager that sits on top of everything
+       else in the site and is rendered as the bar across the
+       screen at the top
+    """      
