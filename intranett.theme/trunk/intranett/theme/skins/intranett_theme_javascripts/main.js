31314c6001403fddb8545523e172404c2379b7a0
Denys Mishunov <denys@jarn.com>
2010-09-09 10:01:36 +0000

    tamming the breadcrumbs. Closes #40

--- intranett.theme/trunk/intranett/theme/skins/intranett_theme_javascripts/main.js
+++ intranett.theme/trunk/intranett/theme/skins/intranett_theme_javascripts/main.js
@@ -1,5 +1,9 @@
 (function($) { 
     $(function() {
+        jQuery.fn.jBreadCrumb.defaults.endElementsToLeaveOpen = 2;
+        jQuery.fn.jBreadCrumb.defaults.beginingElementsToLeaveOpen = 2;
+        jQuery.fn.jBreadCrumb.defaults.maxFinalElementLength = 300;        
+        jQuery.fn.jBreadCrumb.defaults.previewWidth = 15;           
         $("#portal-breadcrumbs").jBreadCrumb(); 
     }); 
 })(jQuery);
