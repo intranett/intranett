9c2fe54fb154698e70edebd62a08ccf15b26efe7
Denys Mishunov <denys@jarn.com>
2010-09-10 14:08:42 +0000

    - a lot of styling updates - dynamic contentMenu/contentActions toggle

--- intranett.theme/trunk/intranett/theme/skins/intranett_theme_javascripts/main.js
+++ intranett.theme/trunk/intranett/theme/skins/intranett_theme_javascripts/main.js
@@ -5,5 +5,17 @@
         jQuery.fn.jBreadCrumb.defaults.maxFinalElementLength = 300;        
         jQuery.fn.jBreadCrumb.defaults.previewWidth = 15;           
         $("#portal-breadcrumbs").jBreadCrumb(); 
+        
+        $("#settings-toggle a").click(function() {
+            // $("#contentviews-wrapper").stop(); 
+            $("#contentviews-wrapper, #contentviews-wrapper + .contentActions").animate({
+                height: ['toggle', 'swing'],                
+                opacity: 'toggle'
+            }, 500, 'linear', function() {
+                // $("#settings-toggle a").fadeOut();
+            });                      
+            // $(this).toggleClass("expandedButton");
+            return false;
+        })
     }); 
 })(jQuery);
