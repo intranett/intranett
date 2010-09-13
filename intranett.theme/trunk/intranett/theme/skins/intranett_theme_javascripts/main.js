297ee8fc833c13afc70c0480d27da74e0299b47f
Denys Mishunov <denys@jarn.com>
2010-09-13 11:47:54 +0000

    JS for contentMenus got simplified

--- intranett.theme/trunk/intranett/theme/skins/intranett_theme_javascripts/main.js
+++ intranett.theme/trunk/intranett/theme/skins/intranett_theme_javascripts/main.js
@@ -6,27 +6,17 @@
         jQuery.fn.jBreadCrumb.defaults.previewWidth = 15;           
         $("#portal-breadcrumbs").jBreadCrumb(); 
         
-        $("#open-edit-bar").click(function() {
+        $("#settings-toggle a").click(function() {
             // $("#contentviews-wrapper").stop(); 
             $("#contentviews-wrapper, #contentviews-wrapper + .contentActions").animate({
                 height: ['toggle', 'swing'],                
                 opacity: 'toggle'
             }, 500, 'linear', function() {
-                $("#open-edit-bar").fadeOut(200);
-            });                      
-            // $(this).toggleClass("expandedButton");
+                var working = 1;
+                var isHidden = $("#open-edit-bar").is(":hidden");
+                isHidden ? $("#open-edit-bar").delay(50).fadeIn(200) : $("#open-edit-bar").delay(50).fadeOut(200);
+            });
             return false;
-        })
-        $("#close-edit-bar").click(function() {
-            // $("#contentviews-wrapper").stop(); 
-            $("#contentviews-wrapper, #contentviews-wrapper + .contentActions").animate({
-                height: ['toggle', 'swing'],                
-                opacity: 'toggle'
-            }, 500, 'linear', function() {
-                $("#open-edit-bar").fadeIn(300);
-            });                     
-            // $(this).toggleClass("expandedButton");
-            return false;
-        })        
+        })      
     }); 
 })(jQuery);
