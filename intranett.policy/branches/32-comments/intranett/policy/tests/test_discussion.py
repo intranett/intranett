aa3bd41b724ecd573a8066b54465a5dca994963d
Hanno Schlichting <hanno@jarn.com>
2010-09-22 12:33:30 +0000

    Enabled discussions for pages, events and news items

--- intranett.policy/branches/32-comments/intranett/policy/tests/test_discussion.py
+++ intranett.policy/branches/32-comments/intranett/policy/tests/test_discussion.py
@@ -22,3 +22,9 @@ class TestSiteSetup(IntranettTestCase):
         self.assertEquals(settings.globally_enabled, False)
         self.assertEquals(settings.show_commenter_image, True)
         self.assertEquals(settings.text_transform, 'text/x-web-intelligent')
+
+    def test_types_enabled(self):
+        tt = getToolByName(self.portal, 'portal_types')
+        self.assertEquals(tt['Document'].allow_discussion, True)
+        self.assertEquals(tt['Event'].allow_discussion, True)
+        self.assertEquals(tt['News Item'].allow_discussion, True)
