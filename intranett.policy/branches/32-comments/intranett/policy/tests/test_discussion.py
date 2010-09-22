0b866a28d056f9b07b0fd1568a5d36c51af6bd77
Hanno Schlichting <hanno@jarn.com>
2010-09-22 10:52:43 +0000

    Added stupid low-level tests for registry defaults

--- intranett.policy/branches/32-comments/intranett/policy/tests/test_discussion.py
+++ intranett.policy/branches/32-comments/intranett/policy/tests/test_discussion.py
@@ -1,4 +1,7 @@
+from plone.app.discussion.interfaces import IDiscussionSettings
+from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
+from zope.component import queryUtility
 
 from intranett.policy.tests.base import IntranettTestCase
 
@@ -10,3 +13,12 @@ class TestSiteSetup(IntranettTestCase):
         comments = [a for a in user.listActions() if a.id == 'review-comments']
         self.assertEquals(len(comments), 1)
         self.assertEquals(comments[0].visible, False)
+
+    def test_registry_defaults(self):
+        registry = queryUtility(IRegistry)
+        settings = registry.forInterface(IDiscussionSettings)
+        self.assertEquals(settings.anonymous_comments, False)
+        self.assertEquals(settings.captcha, 'disabled')
+        self.assertEquals(settings.globally_enabled, False)
+        self.assertEquals(settings.show_commenter_image, True)
+        self.assertEquals(settings.text_transform, 'text/x-web-intelligent')
