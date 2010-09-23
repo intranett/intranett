2496332d1a3f12196cffcb55228eb98358d86e62
Hanno Schlichting <hanno@jarn.com>
2010-09-23 10:08:11 +0000

    No need to specify test suite

--- intranett.theme/trunk/intranett/theme/tests/test_theme.py
+++ intranett.theme/trunk/intranett/theme/tests/test_theme.py
@@ -1,6 +1,3 @@
-from unittest import TestSuite
-from unittest import makeSuite
-
 from Products.CMFCore.utils import getToolByName
 
 from intranett.policy.tests.base import IntranettTestCase
@@ -11,9 +8,3 @@ class TestTheme(IntranettTestCase):
     def test_theme(self):
         skins = getToolByName(self.portal, 'portal_skins')
         self.assertEquals(skins.getDefaultSkin(), 'Intranett.no base theme')
-
-
-def test_suite():
-    return TestSuite([
-        makeSuite(TestTheme),
-    ])
