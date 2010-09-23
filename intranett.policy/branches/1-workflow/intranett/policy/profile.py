eae5a9ad77d95a6eb9b0dfde982be9dec282bf84
Hanno Schlichting <hanno@jarn.com>
2010-09-23 12:28:04 +0000

    Create a branch to work on workflow ticket 1

---
+++ intranett.policy/branches/1-workflow/intranett/policy/profile.py
@@ -0,0 +1,42 @@
+from zope.interface import implements
+
+from Products.CMFPlone.interfaces import INonInstallable
+from Products.CMFQuickInstallerTool.interfaces import INonInstallable as INonQ
+
+
+class HiddenProfiles(object):
+    implements(INonInstallable)
+
+    def getNonInstallableProfiles(self):
+        return [
+            u'borg.localrole:default',
+            u'plone.app.discussion:default',
+            u'plone.app.iterate:plone.app.iterate',
+            u'plone.app.openid:default',
+            u'plone.app.registry:default',
+            u'plone.app.z3cform:default',
+            u'plonetheme.classic:default',
+            u'plonetheme.sunburst:default',
+            u'Products.PloneFormGen:default',
+            u'Products.PloneFormGen:typeoverrides25x',
+            u'intranett.theme:default',
+            ]
+
+class HiddenProducts(object):
+    implements(INonQ)
+
+    def getNonInstallableProducts(self):
+        return [
+            'kupu',
+            'plone.app.discussion',
+            'plone.app.iterate',
+            'plone.app.openid',
+            'plone.app.registry',
+            'plone.app.z3cform',
+            'plonetheme.classic',
+            'Products.CMFPlacefulWorkflow',
+            'Products.kupu',
+            'Products.Marshall',
+            'Products.PloneFormGen',
+            'intranett.theme',
+        ]
