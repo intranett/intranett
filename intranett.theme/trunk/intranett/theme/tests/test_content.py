80c5e541b58959fa629f6d93b0787e99eea238f3
Hanno Schlichting <hanno@jarn.com>
2010-10-04 12:44:26 +0000

    Remove a test that wasn't testing anything we actually wanted to test

--- intranett.theme/trunk/intranett/theme/tests/test_content.py
+++ intranett.theme/trunk/intranett/theme/tests/test_content.py
@@ -1,10 +1,3 @@
-from plone.app.portlets.portlets import navigation
-from plone.portlets.interfaces import IPortletManager
-from plone.portlets.interfaces import IPortletRenderer
-from zope.component import getMultiAdapter
-from zope.component import getUtility
-
-
 from intranett.policy.tests.base import IntranettTestCase
 
 
@@ -17,19 +10,3 @@ class TestContent(IntranettTestCase):
         nav = mapping[u'navigation']
         self.assertEquals(nav.topLevel, 1)
         self.assertEquals(nav.currentFolderOnly, True)
-
-    def test_navigation_portlet_template(self):
-        context = self.portal
-        request = self.portal.REQUEST
-        view = self.portal.restrictedTraverse('@@plone')
-        manager = getUtility(IPortletManager, name='plone.leftcolumn',
-                             context=context)
-        assignment = navigation.Assignment()
-
-        renderer = getMultiAdapter(
-            (context, request, view, manager, assignment),
-            IPortletRenderer)
-
-        html = renderer.render()
-        # We'd really like to test that we use our own template here
-        self.assert_('<dl class="portlet portletNavigationTree">' in html)
