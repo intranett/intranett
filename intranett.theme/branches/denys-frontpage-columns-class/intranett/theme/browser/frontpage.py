16b82d1e5b16594fc33034281cb2d39443294ff5
Hanno Schlichting <hanno@jarn.com>
2010-10-12 13:39:16 +0000

    Fix the compilation error in the template and define the empty dummy view method

--- intranett.theme/branches/denys-frontpage-columns-class/intranett/theme/browser/frontpage.py
+++ intranett.theme/branches/denys-frontpage-columns-class/intranett/theme/browser/frontpage.py
@@ -6,18 +6,20 @@ class FrontpageView(BrowserView):
     """Frontpage view
     """
 
-    # def columns_class(self):
-    #     """ CSS class that is assigned to columns on the frontpage. """
-    #     ploneview = getMultiAdapter((self.context, self.request), name=u'plone')
-    #     
-    #     fp_left = ploneview.have_portlets('frontpage.portlets.left', view)
-    #     fp_central = ploneview.have_portlets('frontpage.portlets.central', view)
-    #     fp_right = ploneview.have_portlets('frontpage.portlets.right', view)
-    #     
-    #     if fp_left and fp_central and fp_right:
-    #         return 'width-1:3'
-    #     elif (fp_left and fp_central) 
-    #          or (fp_left and fp_right) 
-    #          or (fp_central and fp_right):
-    #          return 'width-1:2'
-    #     elif (fp_left)
+    def columns_class(self):
+        """ CSS class that is assigned to columns on the frontpage. """
+        return ''
+
+        # ploneview = getMultiAdapter((self.context, self.request), name=u'plone')
+        # 
+        # fp_left = ploneview.have_portlets('frontpage.portlets.left', view)
+        # fp_central = ploneview.have_portlets('frontpage.portlets.central', view)
+        # fp_right = ploneview.have_portlets('frontpage.portlets.right', view)
+        # 
+        # if fp_left and fp_central and fp_right:
+        #     return 'width-1:3'
+        # elif (fp_left and fp_central) 
+        #      or (fp_left and fp_right) 
+        #      or (fp_central and fp_right):
+        #      return 'width-1:2'
+        # elif (fp_left)
