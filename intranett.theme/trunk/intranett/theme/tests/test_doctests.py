5eb25c3aa9d4cd63772a19b82b5dfb04d723a65b
Hanno Schlichting <hanno@jarn.com>
2010-10-12 10:40:46 +0000

    Added base skeleton for browser tests and add a test requirement on ``intranett.policy``.

--- intranett.theme/trunk/intranett/theme/tests/test_doctests.py
+++ intranett.theme/trunk/intranett/theme/tests/test_doctests.py
@@ -1,17 +1,17 @@
-from unittest import TestSuite
 import doctest
+from unittest import TestSuite
+
 from Testing.ZopeTestCase import ZopeDocFileSuite
+
 from intranett.policy.tests.base import IntranettFunctionalTestCase
 
-
-optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
-               doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+optionflags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
 
 def test_suite():
     suite = TestSuite([
         ZopeDocFileSuite(
-            'tests/frontpage.txt', package='intranett.theme',
+            'frontpage.txt', package='intranett.theme.tests',
             test_class=IntranettFunctionalTestCase,
             optionflags=optionflags)])
     return suite
