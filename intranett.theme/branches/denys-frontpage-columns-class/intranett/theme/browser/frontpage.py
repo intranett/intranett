747872efb680fed76a830f26a0264ae3402d8e77
Denys Mishunov <denys@jarn.com>
2010-10-12 05:51:59 +0000

    half-done function for returning correct CSS class for the frontpage columns. Don't worry about "hlaf-done" - it's branch :)

--- intranett.theme/branches/denys-frontpage-columns-class/intranett/theme/browser/frontpage.py
+++ intranett.theme/branches/denys-frontpage-columns-class/intranett/theme/browser/frontpage.py
@@ -1,3 +1,5 @@
+from zope.component import getMultiAdapter
+
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
@@ -10,3 +12,18 @@ class FrontpageView(BrowserView):
         self.context = context
         self.request = request
 
+    # def columns_class(self):
+    #     """ CSS class that is assigned to columns on the frontpage. """
+    #     ploneview = getMultiAdapter((self.context, self.request), name=u'plone')
+    #     
+    #     fp_left = ploneview.have_portlets('frontpage.portlets.left', view)
+    #     fp_central = ploneview.have_portlets('frontpage.portlets.central', view)
+    #     fp_right = ploneview.have_portlets('frontpage.portlets.right', view)
+    #     
+    #     if fp_left and fp_central and fp_right:
+    #         return 'width-1:3'
+    #     elif (fp_left and fp_central) 
+    #          or (fp_left and fp_right) 
+    #          or (fp_central and fp_right):
+    #          return 'width-1:2'
+    #     elif (fp_left)
