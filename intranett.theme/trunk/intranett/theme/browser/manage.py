afea95a7d51ee39d441c403e5cafebe02fc59f34
Hanno Schlichting <hanno@jarn.com>
2010-10-13 15:31:45 +0000

    A comment is still helpful here

--- intranett.theme/trunk/intranett/theme/browser/manage.py
+++ intranett.theme/trunk/intranett/theme/browser/manage.py
@@ -2,5 +2,6 @@ from plone.app.portlets.browser.manage import ManageContextualPortlets
 
 
 class IntranettManageContextualPortlets(ManageContextualPortlets):
-    def __init__(self, context, request): 
+    def __init__(self, context, request):
+        # Note that we omit our own super class
         super(ManageContextualPortlets, self).__init__(context, request)
