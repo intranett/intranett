18f537ed2d66a544cdcff973bd59b29115560485
Denys Mishunov <denys@jarn.com>
2010-10-13 13:55:33 +0000

    - Re-enabled edit-bar on manage-frontpage view. Test for this is supplied. - Some text changes for "Edit Frontpage" view

--- intranett.theme/trunk/intranett/theme/browser/manage.py
+++ intranett.theme/trunk/intranett/theme/browser/manage.py
@@ -2,4 +2,5 @@ from plone.app.portlets.browser.manage import ManageContextualPortlets
 
 
 class IntranettManageContextualPortlets(ManageContextualPortlets):
-    pass
+    def __init__(self, context, request): 
+        super(ManageContextualPortlets, self).__init__(context, request)
