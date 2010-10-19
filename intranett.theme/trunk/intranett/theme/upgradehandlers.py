c048f26dc82b9963f82236d3a4a3dc9360d58dde
Hanno Schlichting <hanno@jarn.com>
2010-10-19 12:17:53 +0000

    Fixed the profile version to be 3, as the live site deployed based on 0.2 had a version of 2 already.

--- intranett.theme/trunk/intranett/theme/upgradehandlers.py
+++ intranett.theme/trunk/intranett/theme/upgradehandlers.py
@@ -5,6 +5,10 @@ from Products.CMFCore.utils import getToolByName
 logger = logging.getLogger('intranett.theme upgrades')
 
 
+def null_upgrade_step(context):
+    pass
+
+
 def add_media_query_maincss(context):
     css = getToolByName(context, 'portal_css')
     main = css.getResource('main.css')
