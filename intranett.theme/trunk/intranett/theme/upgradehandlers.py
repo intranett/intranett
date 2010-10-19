e9228dcc6e4333912e1ca9d4359ece0eabdd1a59
Hanno Schlichting <hanno@jarn.com>
2010-10-19 09:01:51 +0000

    Pyflakes, PEP8

--- intranett.theme/trunk/intranett/theme/upgradehandlers.py
+++ intranett.theme/trunk/intranett/theme/upgradehandlers.py
@@ -1,15 +1,11 @@
-from zope.component import getUtility, getMultiAdapter, ComponentLookupError
-from zope.component import getSiteManager
-from Products.CMFCore.utils import getToolByName
-from plone.portlets.interfaces import IPortletManager, IPortletAssignmentMapping
-from plone.portlets.interfaces import IPortletType
-
 import logging
-logger  = logging.getLogger('intranett.theme upgrades')
+
+from Products.CMFCore.utils import getToolByName
+
+logger = logging.getLogger('intranett.theme upgrades')
+
 
 def add_media_query_maincss(context):
-    """"""
     css = getToolByName(context, 'portal_css')
     main = css.getResource('main.css')
     main.setMedia("")
-
