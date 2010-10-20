6db298b982513aa2dc710255bfde04ea26fc7140
Hanno Schlichting <hanno@jarn.com>
2010-10-20 14:59:12 +0000

    Whitespace

--- intranett.theme/trunk/intranett/theme/browser/frontpage.py
+++ intranett.theme/trunk/intranett/theme/browser/frontpage.py
@@ -2,18 +2,17 @@ from zope.publisher.browser import BrowserView
 
 
 class FrontpageView(BrowserView):
-    """Frontpage view
-    """
+    """Frontpage view"""
 
-    def columns_class(self):        
+    def columns_class(self):
         ploneview = self.context.restrictedTraverse('@@plone')
         cols = ['frontpage.portlets.left',
                 'frontpage.portlets.central',
                 'frontpage.portlets.right']
-        i = 0        
+        i = 0
         for manager in cols:
             if ploneview.have_portlets(manager, self):
-                i+=1                
+                i+=1
         if i != 0:
             return 'width-%s' % (16/i)
         else:
