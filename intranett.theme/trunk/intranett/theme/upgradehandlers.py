44faffba8bab471bacc6911fb1b942c136d6c45d
Denys Mishunov <denys@jarn.com>
2010-10-21 14:05:51 +0000

    make the instance start up

--- intranett.theme/trunk/intranett/theme/upgradehandlers.py
+++ intranett.theme/trunk/intranett/theme/upgradehandlers.py
@@ -11,6 +11,4 @@ def add_media_query_maincss(context):
     main.setMedia("")
     
 def add_personslisting_action(context):
-    # at = getToolByName(self.portal, 'portal_actions')
-    # tabs = at.portal_tabs
-    # 'persons-listing' in tabs.objectIds()
+    pass
