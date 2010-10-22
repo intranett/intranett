e1e9c7ea8359689c7535020dcc010c16bf2de1ce
Yiorgis Gozadinos <ggozad@jarn.com>
2010-10-22 14:07:26 +0000

    Make denys life easier

--- intranett.theme/trunk/intranett/theme/browser/employeelisting.py
+++ intranett.theme/trunk/intranett/theme/browser/employeelisting.py
@@ -6,7 +6,6 @@ class EmployeeListingView(BrowserView):
     """Employee listing"""
 
     def __init__(self, context, request):
-#        import pdb; pdb.set_trace( )
         super(EmployeeListingView, self).__init__(context, request)
         mt = getToolByName(self.context, 'portal_membership')
         md = getToolByName(self.context, 'portal_memberdata')
@@ -18,6 +17,9 @@ class EmployeeListingView(BrowserView):
             if md.portraits.has_key(member):
                 info['portrait_url'] = "%s/portraits/%s" % (md.absolute_url(), member)
                 info['thumbnail_url'] = "%s/thumbnails/%s" % (md.absolute_url(), member)
+            else:
+                info['portrait_url'] = ''
+                info['thumbnail_url'] = ''
             self.member_info.append(info)
             if info['department']:
                 self.departments.add(info['department'])
