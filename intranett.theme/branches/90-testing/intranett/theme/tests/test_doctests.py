c7b709d98d0ecaf596c1ccffea18064b9fc554b5
Hanno Schlichting <hanno@jarn.com>
2010-11-02 13:57:07 +0000

    Convert doctests over to plone.app.testing, which has no base class anymore

--- intranett.theme/branches/90-testing/intranett/theme/tests/test_doctests.py
+++ intranett.theme/branches/90-testing/intranett/theme/tests/test_doctests.py
@@ -1,30 +1,27 @@
 import doctest
+from doctest import DocFileSuite
 from unittest import TestSuite
 
-from Testing.ZopeTestCase import ZopeDocFileSuite
-
-from intranett.policy.tests.base import IntranettFunctionalTestCase
+from plone.testing import layered
+from intranett.policy.tests.layer import INTRANETT_FUNCTIONAL
 
 optionflags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
 
 def test_suite():
-    suite = TestSuite([
-        ZopeDocFileSuite(
-            'comments.txt', package='intranett.theme.tests',
-            test_class=IntranettFunctionalTestCase,
-            optionflags=optionflags),
-        ZopeDocFileSuite(
-            'employeelisting.txt', package='intranett.theme.tests',
-            test_class=IntranettFunctionalTestCase,
-            optionflags=optionflags),
-        ZopeDocFileSuite(
-            'frontpage.txt', package='intranett.theme.tests',
-            test_class=IntranettFunctionalTestCase,
-            optionflags=optionflags),
-        ZopeDocFileSuite(
-            'robots.txt', package='intranett.theme.tests',
-            test_class=IntranettFunctionalTestCase,
-            optionflags=optionflags),
-        ])
+    suite = TestSuite()
+    suite.addTests([
+        layered(DocFileSuite('comments.txt',
+                             package='intranett.theme.tests'),
+                layer=INTRANETT_FUNCTIONAL),
+        layered(DocFileSuite('employeelisting.txt',
+                             package='intranett.theme.tests'),
+                layer=INTRANETT_FUNCTIONAL),
+        layered(DocFileSuite('frontpage.txt',
+                             package='intranett.theme.tests'),
+                layer=INTRANETT_FUNCTIONAL),
+        layered(DocFileSuite('robots.txt',
+                             package='intranett.theme.tests'),
+                layer=INTRANETT_FUNCTIONAL),
+    ])
     return suite
