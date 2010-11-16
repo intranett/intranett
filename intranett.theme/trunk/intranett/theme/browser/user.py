a10a82557972882eaaf58e8a4499ce275f397550
Hanno Schlichting <hanno@jarn.com>
2010-11-16 15:51:16 +0000

    Factor out catalog query into a method, so we can test it

--- intranett.theme/trunk/intranett/theme/browser/user.py
+++ intranett.theme/trunk/intranett/theme/browser/user.py
@@ -1,3 +1,4 @@
+from Products.CMFCore.utils import getToolByName
 from zope.publisher.browser import BrowserView
 
 
@@ -5,3 +6,13 @@ class UserView(BrowserView):
 
     def username(self):
         return self.request.form.get('name', '')
+
+    def user_content(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        query = {
+            'Creator': self.username(),
+            'sort_on': 'created',
+            'sort_order': 'reverse',
+            'sort_limit': 10,
+        }
+        return catalog.searchResults(query)[:10]
