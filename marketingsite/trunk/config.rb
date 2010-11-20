dcc2b8fbf85de1af07b32230275e0434bd79a793
Denys Mishunov <denys@jarn.com>
2010-11-20 20:43:08 +0000

    adding SCSS sources and Rubby configuration for Compass watching of the project

---
+++ marketingsite/trunk/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+require "lemonade"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "js"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
