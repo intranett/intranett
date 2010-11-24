1c75c1f1a1d09f80950a8b6327ceeb2f0796a8b7
Hanno Schlichting <hanno@jarn.com>
2010-11-24 13:43:10 +0000

    Add uprade step boilerplate for Denys changes

--- intranett.theme/trunk/intranett/theme/upgradehandlers.py
+++ intranett.theme/trunk/intranett/theme/upgradehandlers.py
@@ -9,3 +9,11 @@ def add_media_query_maincss(context):
     css = getToolByName(context, 'portal_css')
     main = css.getResource('main.css')
     main.setMedia("")
+
+
+def add_selectivizr_remove_html_js(context):
+    js = getToolByName(context, 'portal_javascripts')
+    # remove html5.js
+    pass
+    # add selectivizr.js conditionalcomment="lt IE 9" insert-after="jquery.js"
+    pass
