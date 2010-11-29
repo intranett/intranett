cf09cfd07bdc7e9336a5e5855e0df02a27b99fb0
Stefan H. Holek <stefan@jarn.com>
2010-11-29 11:09:23 +0000

    Add decorator for warnings suppression.

--- intranett.policy/branches/90-testing/intranett/policy/tests/utils.py
+++ intranett.policy/branches/90-testing/intranett/policy/tests/utils.py
@@ -1,7 +1,11 @@
+import sys
+
+from StringIO import StringIO
 from ZPublisher.HTTPRequest import FileUpload
 
 
 class DummyFieldStorage(object):
+    """Minimal FieldStorage implementation."""
 
     def __init__(self, data, filename, headers):
         self.file = data
@@ -10,6 +14,8 @@ class DummyFieldStorage(object):
 
 
 def make_file_upload(data, content_type=None, filename=None):
+    """Create a FileUpload object.
+    """
     headers = {}
     if isinstance(data, str):
         data = open(data, 'rb')
@@ -17,3 +23,21 @@ def make_file_upload(data, content_type=None, filename=None):
         headers['content-type'] = content_type
     fs = DummyFieldStorage(data, filename, headers)
     return FileUpload(fs)
+
+
+def suppress_warnings(func):
+    """Decorator suppressing stderr output.
+    """
+    def wrapped_func(*args, **kw):
+        saved = sys.stderr
+        sys.stderr = StringIO()
+        try:
+            return func(*args, **kw)
+        finally:
+            sys.stderr = saved
+
+    wrapped_func.__name__ = func.__name__
+    wrapped_func.__module__ = func.__module__
+    wrapped_func.__doc__ = func.__doc__
+    wrapped_func.__dict__.update(func.__dict__)
+    return wrapped_func
