ff03318925944202d1647adce1bd61116ddb1fb5
Hanno Schlichting <hanno@jarn.com>
2010-12-05 01:17:45 +0000

    Try first useful commands, download fails for mysterious reasons

--- deployments/trunk/etc/deployment/fabfile.py
+++ deployments/trunk/etc/deployment/fabfile.py
@@ -1,4 +1,6 @@
-from fabric.api import cd, env, run, sudo
+import os
+
+from fabric.api import cd, env, run, sudo, settings, hide, get
 
 env.shell = "/bin/bash -c"
 
@@ -9,3 +11,15 @@ def touch():
 def svn_info():
     with cd('/srv/jarn'):
         sudo('pwd && svn info', user='jarn')
+
+def dump_db():
+    with cd('/srv/jarn'):
+        sudo('rm var/snapshotbackups/*', user='jarn')
+        sudo('bin/snapshotbackup', user='jarn')
+
+def download_last_dump():
+    with settings(hide('warnings', 'running', 'stdout', 'stderr'),
+                  warn_only=True):
+        existing = sudo('ls -rt1 /srv/jarn/var/snapshotbackups/*', user='jarn')
+    for e in existing.split('\n'):
+        get(e, os.path.join(os.getcwd(), 'var', 'snapshotbackups'))
