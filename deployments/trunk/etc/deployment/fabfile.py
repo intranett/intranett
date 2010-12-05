147039705e7c87fd933ef99a5e17af2286543c74
Hanno Schlichting <hanno@jarn.com>
2010-12-05 00:01:50 +0000

    Play some more, make this work for my user until Gocept has allowed direct login for the jarn account

--- deployments/trunk/etc/deployment/fabfile.py
+++ deployments/trunk/etc/deployment/fabfile.py
@@ -5,3 +5,7 @@ env.shell = "/bin/bash -c"
 def touch():
     home = run('pwd')
     run('cd {home} && touch iwashere.txt'.format(home=home))
+
+def svn_info():
+    home = run('pwd')
+    run('cd {home} && svn info /srv/jarn'.format(home=home))
