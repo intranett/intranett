4282f8825e89f879da85059559b8f83e7a1b0536
Hanno Schlichting <hanno@jarn.com>
2010-12-05 12:56:18 +0000

    Add cron mailto setup

--- deployments/trunk/etc/deployment/fabfile.py
+++ deployments/trunk/etc/deployment/fabfile.py
@@ -23,12 +23,12 @@ def download_last_dump():
 
 def init_server():
     home = '/home/hannosch'
+    # set up environment variables
     with settings(hide('stdout', 'stderr')):
         profile = run('cat %s/.bash_profile' % home)
     profile_lines = profile.split('\n')
     exports = [l for l in profile_lines if l.startswith('export INTRANETT_')]
     if len(exports) < 2:
-        # set up environment variables
         start, end = profile_lines[:2], profile_lines[2:]
         subdomain = env.host_string
         domain_line = 'export INTRANETT_DOMAIN=%s.intranett.no\n' % subdomain
@@ -38,5 +38,32 @@ def init_server():
         front_line = 'export INTRANETT_ZOPE_IP=%s' % front_ip
 
         new_file = start + [front_line] + [domain_line] + end
-        run('cd {home} && echo -e "{content}" > .bash_profile'.format(
+        run('echo -e "{content}" > {home}/.bash_profile'.format(
             home=home, content='\n'.join(new_file)))
+
+    # set cron mailto
+    with settings(hide('stdout'), warn_only=True):
+        # if no crontab exists, this crontab -l has an exit code of 1
+        run('crontab -l > %s/crontab.tmp' % home)
+        crontab = run('cat %s/crontab.tmp' % home)
+    cron_lines = crontab.split('\n')
+    mailto = [l for l in cron_lines if l.startswith('MAILTO')]
+    if not mailto:
+        # add mailto right after the comments
+        new_cron_lines = []
+        added = False
+        for line in cron_lines:
+            if line.startswith('#'):
+                new_cron_lines.append(line)
+            else:
+                if not added:
+                    # XXX hosting@jarn.com
+                    cron_lines.append('MAILTO=hanno@jarn.com')
+                    added = True
+                new_cron_lines.append(line)
+        with settings(hide('stdout', 'stderr')):
+            run('echo -e "{content}" > {home}/crontab.tmp'.format(
+                home=home, content='\n'.join(new_cron_lines)))
+            run('crontab %s/crontab.tmp' % home)
+    with settings(hide('stdout', 'stderr')):
+        run('rm %s/crontab.tmp' % home)
