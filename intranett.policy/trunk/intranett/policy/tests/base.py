f641bbc8defc651002654aced294622e3940e71e
Stefan H. Holek <stefan@jarn.com>
2010-12-07 20:49:07 +0000

    Import z2 like in layer.py.

--- intranett.policy/trunk/intranett/policy/tests/base.py
+++ intranett.policy/trunk/intranett/policy/tests/base.py
@@ -1,14 +1,13 @@
 import unittest2 as unittest
 
+from plone.testing import z2
 from plone.app.testing import TEST_USER_NAME
 from plone.app.testing import TEST_USER_PASSWORD
-from plone.testing.z2 import Browser
-
 from intranett.policy.tests import layer
 
 
 def get_browser(app, loggedIn=True):
-    browser = Browser(app)
+    browser = z2.Browser(app)
     if loggedIn:
         auth = 'Basic %s:%s' % (TEST_USER_NAME, TEST_USER_PASSWORD)
         browser.addHeader('Authorization', auth)
