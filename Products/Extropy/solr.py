0bc596491cfc23c9db11195750b8fb235242ec81
Hanno Schlichting <hanno@jarn.com>
2011-01-10 21:00:29 +0000

    Tweak solr settings

--- Products/Extropy/solr.py
+++ Products/Extropy/solr.py
@@ -9,19 +9,28 @@ solr_config.host = '127.0.0.1'
 solr_config.port = 10093
 solr_config.base = '/solr'
 solr_config.async = True
-solr_config.index_timeout = 0.0
-solr_config.search_timeout = 0.0
-solr_config.max_results = 500
-solr_config.required = ('SearchableText', 'facet', 'use_solr', )
+solr_config.auto_commit = False
+solr_config.commit_within = 60000 # 60 seconds
+solr_config.effective_steps = 900 # 15 minutes
+solr_config.index_timeout = 30.0
+solr_config.search_pattern = \
+    '(Title:{value}^5 OR Description:{value}^2 OR SearchableText:{value} ' \
+    'OR SearchableText:{base_value})'
+solr_config.search_timeout = 30.0
+solr_config.slow_query_threshold = 1000 # 1 second
+solr_config.max_results = 100
+solr_config.required = ('SearchableText', 'facet', )
 solr_config.facets = ('portal_type', )
 solr_config.filter_queries = (
+    'allowedRolesAndUsers effective expires',
+    'portal_type review_state',
     'allowedRolesAndUsers',
     'effective',
     'expires',
     'is_default_page',
     'object_provides',
-    'path',
     'parentPaths',
+    'path',
     'physicalDepth',
     'portal_type',
     'review_state',
@@ -29,21 +38,3 @@ solr_config.filter_queries = (
 )
 
 solr_test_config = copy(solr_config)
-
-from collective.solr import mangler
-from collective.solr.search import quote
-from DateTime import DateTime
-
-def lowres_convert(value):
-    """ convert values, which need a special format, i.e. dates """
-    if isinstance(value, DateTime):
-        v = value.toZone('UTC')
-        # Lower date resolution to multiples of 15 minutes
-        minute = v.minute() / 15 * 15
-        value = '%04d-%02d-%02dT%02d:%02d:000Z' % (v.year(),
-            v.month(), v.day(), v.hour(), minute)
-    elif isinstance(value, basestring):
-        value = quote(value)
-    return value
-
-mangler.convert = lowres_convert
