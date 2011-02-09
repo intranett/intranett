bd00c4d9b6cc2a7d255cce8132d0fce8ab54618f
Hanno Schlichting <hanno@jarn.com>
2011-02-09 16:29:57 +0100

    Added upgrade step for existing portraits to new class

--- src/intranett.policy/intranett/policy/upgrades/tests/test_steps.py
+++ src/intranett.policy/intranett/policy/upgrades/tests/test_steps.py
@@ -1,6 +1,12 @@
+import os
+
+from OFS.Image import Image
+from plone.app.testing import TEST_USER_ID
 from Products.CMFCore.utils import getToolByName
 
 from intranett.policy.tests.base import IntranettTestCase
+from intranett.policy.tests.test_userdata import TEST_IMAGES
+from intranett.policy.tests.utils import make_file_upload
 
 
 class TestUpgradeSteps(IntranettTestCase):
@@ -66,3 +72,24 @@ class TestUpgradeSteps(IntranettTestCase):
         self.assertEqual(registry.records[purge_key].value,
             ('File', 'Image', 'News Item'))
         self.assertTrue('editbar' in registry.records[etag_key].value)
+
+    def test_migrate_portraits(self):
+        from ..steps import migrate_portraits
+        from intranett.policy.tools import Portrait
+        portal = self.layer['portal']
+
+        mdt = getToolByName(portal, 'portal_memberdata')
+        path = os.path.join(TEST_IMAGES, 'test.jpg')
+        image_jpg = make_file_upload(path, 'image/jpeg', 'myportrait.jpg')
+
+        image = Image(id=TEST_USER_ID, file=image_jpg, title='')
+        thumb = Image(id=TEST_USER_ID, file=image_jpg, title='')
+        mdt._setPortrait(image, TEST_USER_ID)
+        mdt._setPortrait(thumb, TEST_USER_ID, thumbnail=True)
+
+        migrate_portraits(portal)
+
+        self.assertTrue(TEST_USER_ID in mdt.portraits)
+        self.assertTrue(TEST_USER_ID in mdt.thumbnails)
+        self.assertTrue(isinstance(mdt.portraits[TEST_USER_ID], Portrait))
+        self.assertTrue(isinstance(mdt.thumbnails[TEST_USER_ID], Portrait))
