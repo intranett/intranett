f029afeaad533fc093a607c73ab573a19c6fe294
Stefan H. Holek <stefan@jarn.com>
2011-02-09 10:58:34 +0100

    Add an editbar ETag component.

---
+++ src/intranett.policy/intranett/policy/caching.py
@@ -0,0 +1,22 @@
+from zope.interface import implements
+from zope.interface import Interface
+
+from zope.component import adapts
+
+from plone.app.caching.interfaces import IETagValue
+
+
+class EditBarCookie(object):
+    """The ``editbar`` etag component, returning the value of the
+    editbar_opened cookie.
+    """
+
+    implements(IETagValue)
+    adapts(Interface, Interface)
+
+    def __init__(self, published, request):
+        self.published = published
+        self.request = request
+
+    def __call__(self):
+        return self.request.cookies.get('editbar_opened', '0')
