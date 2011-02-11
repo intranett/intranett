d78309f77d934e4350258872f946a1d8b853a037
Martijn Pieters <mj@jarn.com>
2011-02-11 10:33:46 +0100

    Add some more exceptions for iw.rejectanonymous
    I found these by disabling Unauthorized in the error log.

--- src/intranett.policy/intranett/policy/patches.py
+++ src/intranett.policy/intranett/policy/patches.py
@@ -1,6 +1,9 @@
 def allow_anonymous_robotstxt():
-    from iw.rejectanonymous import addValidIds
+    from iw.rejectanonymous import addValidIds, addValidSubparts
     addValidIds('robots.txt')
+    addValidIds('logged_out')
+    addValidIds('spinner.png')
+    addValidSubparts('portal_kss')
 
 
 def apply():
