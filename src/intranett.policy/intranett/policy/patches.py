6fd8cdf4577b0420c406999fa376c039aaa6e5ad
Martijn Pieters <mj@jarn.com>
2011-02-11 11:20:06 +0100

    No need to allow spinner.png if spinner.gif is used instead

--- src/intranett.policy/intranett/policy/patches.py
+++ src/intranett.policy/intranett/policy/patches.py
@@ -2,7 +2,6 @@ def allow_anonymous_robotstxt():
     from iw.rejectanonymous import addValidIds, addValidSubparts
     addValidIds('robots.txt')
     addValidIds('logged_out')
-    addValidIds('spinner.png')
     addValidSubparts('portal_kss')
 
 
