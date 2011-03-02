7134e1cf2dca73431ff4185fcd518545768bbc62
Yiorgis Gozadinos <ggozad@jarn.com>
2011-03-02 11:01:00 +0100

    Test coverage for eventhighlight portlet

--- src/intranett.policy/intranett/policy/tests/test_portlets.py
+++ src/intranett.policy/intranett/policy/tests/test_portlets.py
@@ -106,7 +106,8 @@ class TestNewsHighlightPortlet(IntranettTestCase):
             data={'portletTitle': 'News Highlight', 'source': 'last'})
         self.assertEquals(len(mapping), 1)
         self.failUnless(
-            isinstance(mapping.values()[0],newshighlight.Assignment))
+            isinstance(mapping.values()[0], newshighlight.Assignment))
+
 
 class TestEventHighlightPortlet(IntranettTestCase):
 
@@ -125,6 +126,18 @@ class TestEventHighlightPortlet(IntranettTestCase):
         portal = self.layer['portal']
         setRoles(portal, TEST_USER_ID, ['Contributor'])
         wt = getToolByName(portal, 'portal_workflow')
+
+        # No event
+        assignment = eventhighlight.Assignment(
+            portletTitle="Event")
+        r = self.renderer(assignment=assignment)
+        r = r.__of__(portal)
+        r.update()
+        self.assertEqual(r.item(), None)
+        output = r.render()
+        self.assertTrue('Event' not in output)
+
+        # Add two events, show the last.
         tomorrow = DateTime() + 1
         in_a_month = tomorrow + 30
         portal.invokeFactory('Event', 'wedding',
@@ -141,11 +154,24 @@ class TestEventHighlightPortlet(IntranettTestCase):
         r = self.renderer(assignment=assignment)
         r = r.__of__(portal)
         r.update()
-
         self.assertEqual(r.item().Title, 'A wedding')
         output = r.render()
         self.assertTrue('Event' in output)
         self.assertTrue('A wedding' in output)
+
+    def test_invoke_add_view(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        portlet = getUtility(IPortletType,
+            name='intranett.policy.portlets.EventHighlight')
+        mapping = portal.restrictedTraverse(
+            '++contextportlets++plone.rightcolumn')
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+        addview.createAndAdd(
+            data={'portletTitle': 'Event Highlight'})
+        self.assertEquals(len(mapping), 1)
+        self.failUnless(
+            isinstance(mapping.values()[0], eventhighlight.Assignment))
 
 
 class TestContentHighlightPortlet(IntranettTestCase):
