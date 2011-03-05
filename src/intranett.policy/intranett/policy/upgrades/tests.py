dd483c8dbe008d71f11695febc49b90a2d020582
Hanno Schlichting <hanno@jarn.com>
2011-03-05 19:00:06 +0100

    Rewrite remaining upgrade steps to new after_x approach

--- src/intranett.policy/intranett/policy/upgrades/tests.py
+++ src/intranett.policy/intranett/policy/upgrades/tests.py
@@ -3,12 +3,10 @@ import os
 from plone.app.testing import TEST_USER_ID
 from plone.portlets.interfaces import IPortletManager
 from plone.portlets.interfaces import IPortletType
-from plone.portlets.manager import PortletManager
 from Products.CMFCore.utils import getToolByName
 from zope.component import getSiteManager
 from zope.component import queryUtility
 
-from intranett.policy.upgrades import steps
 from intranett.policy.tests.base import IntranettFunctionalTestCase
 from intranett.policy.tests.upgrade import UpgradeTests
 
@@ -31,11 +29,10 @@ class TestUpgradeSteps(UpgradeTests, IntranettFunctionalTestCase):
             ('File', 'Image', 'News Item'))
         self.assertTrue('editbar' in registry.records[etag_key].value)
 
-    def after_8(self):
+    def before_8(self):
         from OFS.Image import Image
         from intranett.policy.tests.test_userdata import TEST_IMAGES
         from intranett.policy.tests.utils import make_file_upload
-        from intranett.policy.tools import Portrait
 
         portal = self.layer['portal']
 
@@ -48,8 +45,10 @@ class TestUpgradeSteps(UpgradeTests, IntranettFunctionalTestCase):
         mdt._setPortrait(image, TEST_USER_ID)
         mdt._setPortrait(thumb, TEST_USER_ID, thumbnail=True)
 
-        steps.migrate_portraits(portal)
-
+    def after_8(self):
+        from intranett.policy.tools import Portrait
+        portal = self.layer['portal']
+        mdt = getToolByName(portal, 'portal_memberdata')
         self.assertTrue(TEST_USER_ID in mdt.portraits)
         self.assertTrue(TEST_USER_ID in mdt.thumbnails)
         self.assertTrue(isinstance(mdt.portraits[TEST_USER_ID], Portrait))
@@ -58,22 +57,10 @@ class TestUpgradeSteps(UpgradeTests, IntranettFunctionalTestCase):
     def after_9(self):
         portal = self.layer['portal']
         ptool = getToolByName(portal, 'portal_properties')
-        sprops = ptool.site_properties
-        sprops.webstats_js = '<script type="text/javascript" />'
-        steps.disable_webstats_js(portal)
-        self.assertEqual(sprops.webstats_js, '')
+        self.assertEqual(ptool.site_properties.webstats_js, '')
 
     def after_10(self):
-        portal = self.layer['portal']
         sm = getSiteManager()
-        names = ('frontpage.portlets.left', 'frontpage.portlets.central',
-            'frontpage.bottom')
-        def create_pm(name):
-            sm.registerUtility(component=PortletManager(),
-                provided=IPortletManager, name=name)
-        for name in names:
-            create_pm(name)
-        steps.remove_unused_frontpage_portlets(portal)
         registrations = [r.name for r in sm.registeredUtilities()
                          if IPortletManager == r.provided]
         self.assertFalse('frontpage.portlets.left' in registrations)
@@ -83,53 +70,36 @@ class TestUpgradeSteps(UpgradeTests, IntranettFunctionalTestCase):
     def after_11(self):
         portal = self.layer['portal']
         existing_roles = set(getattr(portal, '__ac_roles__', []))
-        existing_roles.remove('Site Administrator')
-        portal.__ac_roles__ = tuple(existing_roles)
-        steps.add_site_administrator(portal)
-        existing_roles = set(getattr(portal, '__ac_roles__', []))
         self.assertIn('Site Administrator', existing_roles)
 
     def after_12(self):
-        from plone.app.portlets.interfaces import IColumn
         portal = self.layer['portal']
-        setattr(portal, '_Portlets__Manage_portlets_Permission', ['Manager'])
-        fti = getToolByName(portal, 'portal_types')['Plone Site']
-        edit_action = [a for a in fti.listActions() if a.id == 'edit-frontpage']
-        edit_action[0].permissions = ('Manage portal', )
-        coll_id = u'plone.portlet.collection.Collection'
-        coll = queryUtility(IPortletType, name=coll_id)
-        coll.for_ = [IColumn]
-        steps.allow_site_admin_to_edit_frontpage(portal)
         perms = set(getattr(portal, '_Portlets__Manage_portlets_Permission'))
         self.assertEqual(perms, set(['Manager', 'Site Administrator']))
+        fti = getToolByName(portal, 'portal_types')['Plone Site']
+        edit_action = [a for a in fti.listActions() if a.id == 'edit-frontpage']
         self.assertEqual(edit_action[0].permissions,
             (u'Portlets: Manage portlets', ))
+        coll_id = u'plone.portlet.collection.Collection'
+        coll = queryUtility(IPortletType, name=coll_id)
         self.assertEqual(coll.for_, [])
 
     def after_13(self):
         portal = self.layer['portal']
         perm_id = '_plone_portlet_static__Add_static_portlet_Permission'
-        setattr(portal, perm_id, ['Manager'])
-        steps.allow_member_to_edit_personal_portlets(portal)
         self.assertEqual(set(getattr(portal, perm_id)),
             set(['Manager', 'Member', 'Site Administrator']))
 
     def after_14(self):
         from plone.caching.interfaces import ICacheSettings
         from plone.registry.interfaces import IRegistry
-        portal = self.layer['portal']
-        steps.add_frontpage_cacherule(portal)
         registry = queryUtility(IRegistry)
         settings = registry.forInterface(ICacheSettings)
         value = settings.operationMapping['intranett.frontpage']
         self.assertEqual(value, 'plone.app.caching.noCaching')
 
     def after_15(self):
-        portal = self.layer['portal']
         sm = getSiteManager()
-        sm.registerUtility(component=PortletManager(),
-            provided=IPortletManager, name='frontpage.highlight')
-        steps.change_frontpage_portlets(portal)
         registrations = [r.name for r in sm.registeredUtilities()
                          if IPortletManager == r.provided]
         self.assertFalse('frontpage.highlight' in registrations)
@@ -138,42 +108,26 @@ class TestUpgradeSteps(UpgradeTests, IntranettFunctionalTestCase):
     def after_16(self):
         portal = self.layer['portal']
         perm_id = '_ATContentTypes__Add_Folder_Permission'
-        setattr(portal, perm_id, ['Manager'])
-        steps.allow_siteadmin_to_edit_content(portal)
         self.assertEqual(set(getattr(portal, perm_id)),
             set(['Manager', 'Contributor', 'Site Administrator', 'Owner']))
 
     def after_17(self):
         portal = self.layer['portal']
-        prefix = '++resource++plone.formwidget.autocomplete/jquery.' \
-            'autocomplete'
-        css = getToolByName(portal, 'portal_css')
-        css.unregisterResource(prefix + '.css')
-        js = getToolByName(portal, 'portal_javascripts')
-        js.unregisterResource(prefix + '.min.js')
         sm = getSiteManager()
-        sm.unregisterUtility(provided=IPortletType,
-            name='intranett.policy.portlets.NewsHighlight')
-        sm.unregisterUtility(provided=IPortletType,
-            name='intranett.policy.portlets.EventHighlight')
-        regs = [r.name for r in sm.registeredUtilities()
-            if r.provided == IPortletType]
-        # call the upgrade step
-        steps.install_highlight_portlets(portal)
         regs = [r.name for r in sm.registeredUtilities()
             if r.provided == IPortletType]
         self.assertTrue('intranett.policy.portlets.NewsHighlight' in regs)
         self.assertTrue('intranett.policy.portlets.EventHighlight' in regs)
+        prefix = '++resource++plone.formwidget.autocomplete/jquery.' \
+            'autocomplete'
+        css = getToolByName(portal, 'portal_css')
         self.assertTrue(prefix + '.css' in css.getResourcesDict().keys())
+        js = getToolByName(portal, 'portal_javascripts')
         self.assertTrue(prefix + '.min.js' in js.getResourcesDict().keys())
 
     def after_18(self):
-        from Products.PluginIndexes.FieldIndex.FieldIndex import FieldIndex
         portal = self.layer['portal']
         atct = getToolByName(portal, 'portal_atct')
-        atct.addIndex('flaggedobject')
         catalog = getToolByName(portal, 'portal_catalog')
-        catalog._catalog.addIndex('flaggedobject', FieldIndex('flaggedobject'))
-        steps.deactivate_collective_flag(portal)
         self.assertFalse('flaggedobject' in atct.getIndexes())
         self.assertFalse('flaggedobject' in catalog.indexes())
