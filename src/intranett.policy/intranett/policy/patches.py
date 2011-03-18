86e564eef7b9297f9607ea504e44720dd1d3ba57
Hanno Schlichting <hanno@jarn.com>
2011-03-18 14:16:04 +0100

    Add new mime.types to the Python mimetypes module and correct mimetype for woff while the ietf request is being processed

--- src/intranett.policy/intranett/policy/patches.py
+++ src/intranett.policy/intranett/policy/patches.py
@@ -5,5 +5,13 @@ def allow_anonymous_robotstxt():
     addValidSubparts('portal_kss')
 
 
+def add_mime_types():
+    import os
+    from zope.contenttype import add_files
+    here = os.path.dirname(os.path.abspath(__file__))
+    add_files([os.path.join(here, "mime.types")])
+
+
 def apply():
     allow_anonymous_robotstxt()
+    add_mime_types()
