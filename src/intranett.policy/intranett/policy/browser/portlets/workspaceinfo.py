b5f2d6fdab7f9c76ef20aab28dd98803c97fcb0f
Matthew Wilkes <wilkes@jarn.com>
2011-05-05 12:50:37 +0200

    Pass over to stefan

--- src/intranett.policy/intranett/policy/browser/portlets/workspaceinfo.py
+++ src/intranett.policy/intranett/policy/browser/portlets/workspaceinfo.py
@@ -1,9 +1,13 @@
 from plone.app.portlets.portlets import base
+from plone.formwidget.autocomplete import AutocompleteFieldWidget
 from plone.portlets.interfaces import IPortletDataProvider
+from plone.principalsource.source import UsersVocabularyFactory
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.formlib import form
+from z3c.form import button, field, form
+from zope.formlib import form as formlibform
 from zope.interface import implements
+import zope.schema
 
 from intranett.policy import IntranettMessageFactory as _
 
@@ -22,6 +26,9 @@ class Assignment(base.Assignment):
 class Renderer(base.Renderer):
 
     render = ViewPageTemplateFile('workspaceinfo.pt')
+    username = zope.schema.Choice(title=_(u"User"),
+        description=_(u"Select the user to be added"),
+        source=UsersVocabularyFactory)
 
     @property
     def available(self):
@@ -40,12 +47,14 @@ class Renderer(base.Renderer):
         members = self.context.members
         members = map(self.context.portal_membership.getMemberById, members)
         self.members = tuple(member.getProperty("fullname") for member in members)
+        import pdb; pdb.set_trace( )
+        NotImplemented
 
 
 
 class AddForm(base.AddForm):
 
-    form_fields = form.Fields(IWorkspaceInfo)
+    form_fields = formlibform.Fields(IWorkspaceInfo)
 
     def create(self, data):
         return Assignment()
@@ -53,4 +62,5 @@ class AddForm(base.AddForm):
 
 class EditForm(base.EditForm):
 
-    form_fields = form.Fields(IWorkspaceInfo)
+    form_fields = formlibform.Fields(IWorkspaceInfo)
+
