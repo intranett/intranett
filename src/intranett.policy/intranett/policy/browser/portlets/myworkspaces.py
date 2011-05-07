33a5ac6982c15f93a1308819935a2af7407292d5
Stefan H. Holek <stefan@jarn.com>
2011-05-07 23:20:29 +0200

    Add myworkspaces portlet.

---
+++ src/intranett.policy/intranett/policy/browser/portlets/myworkspaces.py
@@ -0,0 +1,51 @@
+from plone.app.portlets.portlets import base
+from plone.portlets.interfaces import IPortletDataProvider
+from AccessControl import getSecurityManager
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.formlib import form as formlibform
+from zope.interface import implements
+
+from intranett.policy import IntranettMessageFactory as _
+
+
+class IMyWorkspaces(IPortletDataProvider):
+    """Provides a list of all of my workspaces"""
+
+
+class Assignment(base.Assignment):
+
+    implements(IMyWorkspaces)
+
+    title = _("My Workspaces")
+
+
+class Renderer(base.Renderer):
+
+    render = ViewPageTemplateFile('myworkspaces.pt')
+
+    @property
+    def available(self):
+        return True
+
+    @property
+    def portletTitle(self):
+        return _("My Workspaces")
+
+    def update(self):
+        if not self.available:
+            return
+        self.workspaces = ()
+
+
+class AddForm(base.AddForm):
+
+    form_fields = formlibform.Fields(IMyWorkspaces)
+
+    def create(self, data):
+        return Assignment()
+
+
+class EditForm(base.EditForm):
+
+    form_fields = formlibform.Fields(IMyWorkspaces)
