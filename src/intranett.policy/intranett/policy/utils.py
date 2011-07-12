74bb40ab9abfeed6901d1697c05d9f3b8fd8588f
Hanno Schlichting <hanno@jarn.com>
2011-07-12 10:41:56 +0200

    Removed ZODB cache warming code - it didn't actually help in practice, as the first request to render the Plone site didn't reuse the warmed connection but opened a new one

--- src/intranett.policy/intranett/policy/utils.py
+++ src/intranett.policy/intranett/policy/utils.py
@@ -5,11 +5,7 @@ from Acquisition import aq_get
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import _createObjectByType
 from Products.PlonePAS.utils import cleanId
-from ZODB.POSException import POSKeyError
-from ZODB.utils import p64
-from zope.component import adapter
 from zope.component import queryUtility
-from zope.processlifetime import IProcessStarting
 
 from intranett.policy.config import PERSONAL_FOLDER_ID
 from intranett.policy.interfaces import IMembersFolderId
@@ -105,30 +101,6 @@ def create_personal_folder(context, user_id):
         folder.manage_setLocalRoles(user_id, ['Owner'])
         folder.setCreators([user_id])
         folder.reindexObject()
-
-
-@adapter(IProcessStarting)
-def warmupZODBCache(event): # pragma: no cover
-    logger.info('Warming up ZODB cache.')
-    import Zope2
-    cache_size = Zope2.DB.getCacheSize()
-    with Zope2.DB.transaction() as conn:
-        loaded = 0
-        for i in range(1, cache_size * 2):
-            try:
-                obj = conn.get(p64(i))
-            except POSKeyError:
-                continue
-            obj._p_activate()
-            loaded += 1
-            if loaded >= cache_size:
-                break
-        sites = conn.root()['Application'].objectValues('Plone Site')
-        if sites:
-            site = sites[0]
-            path = {'query': '/'.join(site.getPhysicalPath()), 'depth': 1}
-            site.portal_catalog({'path': path, 'sort_on': 'path'})
-    logger.info('Warmed up ZODB cache with %d items.' % loaded)
 
 
 # Make functions available to scripts
