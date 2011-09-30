3812bc5f80a9ebfa3901b9ee377453aa3e88458b
Hanno Schlichting <hanno@jarn.com>
2011-09-30 11:36:57 +0200

    Remove the defer from the zenbox.js

--- src/intranett.policy/intranett/policy/browser/support.py
+++ src/intranett.policy/intranett/policy/browser/support.py
@@ -4,12 +4,12 @@ from zope.viewlet.interfaces import IViewlet
 
 
 VIEWLET_TEXT = u"""
-<script type="text/javascript" defer="defer"
+<script type="text/javascript"
   src="//asset0.zendesk.com/external/zenbox/v2.1/zenbox.js"></script>
 <style type="text/css" media="screen, projection">
   @import url(//asset0.zendesk.com/external/zenbox/v2.1/zenbox.css);
 </style>
-<script type="text/javascript" defer="defer">
+<script type="text/javascript">
   if (typeof(Zenbox) !== "undefined") {
     Zenbox.init({
       dropboxID:   "20021871",
