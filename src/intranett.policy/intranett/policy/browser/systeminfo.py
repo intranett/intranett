e812917a0449ad14637f911ed5bbe7c9029fb44d
Hanno Schlichting <hanno@jarn.com>
2011-10-11 09:04:20 +0200

    Internal: Expand system-info view to print out statistics on number of users, content items and modified items.

--- src/intranett.policy/intranett/policy/browser/systeminfo.py
+++ src/intranett.policy/intranett/policy/browser/systeminfo.py
@@ -1,5 +1,9 @@
 import json
 
+from Acquisition import aq_get
+from BTrees.IIBTree import multiunion
+from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
 from zope.publisher.browser import BrowserView
 
 from intranett.policy.config import config
@@ -8,7 +12,24 @@ from intranett.policy.config import config
 class SystemInfo(BrowserView):
 
     def __call__(self):
-        output = {'version': config.package_version}
-        self.request.response.setHeader('content-type', 'application/json')
-        self.request.response.setBody(json.dumps(output))
-        return self.request.response
+        context = self.context
+        acl_users = aq_get(context, 'acl_users')
+        catalog = getToolByName(context, 'portal_catalog')
+        now = DateTime()
+        modified = catalog._catalog.indexes['modified']
+        mod_values = modified._index.values
+        days_7 = modified._convert(now - 7)
+        days_14 = modified._convert(now - 14)
+        days_30 = modified._convert(now - 30)
+        output = {
+            'version': config.package_version,
+            'objects': len(catalog),
+            'users': len(acl_users.source_users.listUserIds()),
+            'modified_30': len(multiunion(mod_values(days_30))),
+            'modified_14': len(multiunion(mod_values(days_14))),
+            'modified_7': len(multiunion(mod_values(days_7))),
+        }
+        response = self.request.response
+        response.setHeader('content-type', 'application/json')
+        response.setBody(json.dumps(output))
+        return response
