7a73a987df4460d77184256e42af1215f921916b
Martin Opstad Reistadbakk <martior@jarn.com>
2011-10-13 13:58:02 +0200

    now uploads file

--- src/intranett.policy/intranett/policy/browser/controlpanels/settings.py
+++ src/intranett.policy/intranett/policy/browser/controlpanels/settings.py
@@ -7,6 +7,8 @@ from zope.formlib import form
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFDefault.formlib.schema import SchemaAdapterBase
+from Products.CMFDefault.formlib.widgets import FileUploadWidget
+from Products.CMFDefault.formlib.schema import FileUpload
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from plone.app.controlpanel.form import ControlPanelForm
 from Products.CMFPlone.utils import safe_unicode
@@ -15,17 +17,26 @@ from intranett.policy import IntranettMessageFactory
 
 _ = IntranettMessageFactory
 
-
+LOGO_ID = "customer_logo.png"
 
 class ISettings(Interface):
     """Global visual settings.
     """
 
+
+    customer_logo = FileUpload(title=_(u"Logo"),
+                                 description=_(u"help_customer_logo",
+                                               default=u"Add your logo"),
+                                 required=False,
+                                 default=None)
+
     site_title = schema.TextLine(title=_(u"Intranet name"),
-                                  description=_(u"help_akismet_key_site",
-                                                default=u"Enter the name of your intranett, this will be used as the title of your intranet and also in place of a logo if you do not have a logo"),
-                                  required=False,
-                                  default=u'',)
+                                 description=_(u"help_site_title",
+                                               default=u"Enter the name of your intranett, this will be used as the title of your intranet and also in place of a logo if you do not have a logo"),
+                                 required=False,
+                                 default=u'',)
+
+
 
 class SettingsAdapter(SchemaAdapterBase):   
     adapts(IPloneSiteRoot)
@@ -44,15 +55,30 @@ class SettingsAdapter(SchemaAdapterBase):
     def set_site_title(self, value):
         self.portal.title = value.encode(self.encoding)
 
+    def get_customer_logo(self):
+        image = getattr(self.portal.portal_skins.custom,'customer_logo.png',None)
+        return image
+    
+    def set_customer_logo(self, value):
+        if value is None or value =="":
+            return
+        custom = self.portal.portal_skins.custom
+        if LOGO_ID in custom:
+            custom.manage_delObjects(LOGO_ID)
+        custom.manage_addImage(LOGO_ID,value)
 
+    customer_logo = property(get_customer_logo, set_customer_logo)
     site_title = property(get_site_title, set_site_title)
 
  
 class SettingsControlPanel(ControlPanelForm):
 
     form_fields = form.FormFields(ISettings)
-    
+    form_fields['customer_logo'].custom_widget = FileUploadWidget
     label = _("Settings")
     description = _("")
     form_name = _("")
+
+
+        
 
