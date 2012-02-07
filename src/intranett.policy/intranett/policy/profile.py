ac6cae6883888433df5f2b29f5a22cad709aa607
Martin Opstad Reistadbakk <martior@jarn.com>
2012-02-07 13:37:27 +0100

    don't hide clamav

--- src/intranett.policy/intranett/policy/profile.py
+++ src/intranett.policy/intranett/policy/profile.py
@@ -3,7 +3,7 @@ from zope.interface import implements
 from Products.CMFQuickInstallerTool.interfaces import INonInstallable as INonQ
 
 
-HIDDEN = ['collective.ATClamAV', 'plone.app.caching',
+HIDDEN = ['plone.app.caching',
     'plone.app.discussion', 'plone.app.openid', 'plone.app.registry',
     'plone.app.z3cform', 'plone.formwidget.autocomplete', 'plone.session',
     'plonetheme.classic', 'Products.PloneInvite', 'Products.PloneFormGen',
